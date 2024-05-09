baa78376303de44039f71ee1f59729d9afae579a
Seth Chamberlin <s-chamberlin@onu.edu>
2024-05-09 10:09:07 -0400

    13b4

--- Lab 13-b/Lab13-b.cpp
+++ Lab 13-b/Lab13-b.cpp
@@ -7,39 +7,52 @@ int dayOfWeek(int month, int day, int year);
 int daysInMonth(int month, int year);
 bool isLeapYear(int year);
 
-const string dayNames[7] = { "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+const string dayNames[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 const string monthNames[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+const string calendar[] = {
+    " Su Mo Tu We Th Fr Sa ",
+    "    1  2  3  4  5  6 ",
+    "  7  8  9 10 11 12 13 ",
+    " 14 15 16 17 18 19 20 ",
+    " 21 22 23 24 25 26 27 ",
+    " 28 29 30 31          "
+};
 
 int main() {
     string input;
     int month;
-    int day;
     int year;
 
     do {
-        cout << "Enter a date or Q to quit: ";
+        cout << "Enter a month and year or Q to quit: ";
         getline(cin, input);
         if (input == "Q" || input == "q") {
             break;
         }
 
-        size_t pos1 = input.find(' ');
-        size_t pos2 = input.find(' ', pos1 + 1);
-        if (pos1 == string::npos || pos2 == string::npos) {
+        size_t pos = input.find(' ');
+        if (pos == string::npos) {
             cout << "Invalid input format.\n";
             continue;
         }
 
-        month = stoi(input.substr(0, pos1));
-        day = stoi(input.substr(pos1 + 1, pos2 - pos1 - 1));
-        year = stoi(input.substr(pos2 + 1));
+        month = stoi(input.substr(0, pos));
+        year = stoi(input.substr(pos + 1));
 
-        if (month < 1 || month > 12 || day < 1 || day > daysInMonth(month, year) || year < 1) {
-            cout << "Invalid date.\n";
+        if (month < 1 || month > 12 || year < 1) {
+            cout << "Invalid month or year.\n";
             continue;
         }
 
-        cout << dayNames[dayOfWeek(month, day, year)] << ", " << monthNames[month - 1] << " " << day << ", " << year << endl;
+        cout << monthNames[month - 1] << " " << year << endl;
+
+        int firstDayOfWeek = dayOfWeek(month, 1, year);
+
+        for (int i = 0; i < 6; ++i) {
+            cout << calendar[i].substr(3 * firstDayOfWeek, 21) << endl;
+            firstDayOfWeek = 0; // For subsequent lines, start from Sunday
+        }
+
     } while (true);
 
     return 0;
@@ -60,22 +73,7 @@ int daysInMonth(int month, int year) {
 }
 
 bool isLeapYear(int year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return true;
-        }
-    }
-    else {
-        return false;
-    }
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
 int dayOfWeek(int month, int day, int year) {

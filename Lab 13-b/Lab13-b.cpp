0e769f29157f38f019511bd00409fdcf69ffe12e
Seth Chamberlin <s-chamberlin@onu.edu>
2024-05-09 09:33:52 -0400

    13b

--- Lab 13-b/Lab13-b.cpp
+++ Lab 13-b/Lab13-b.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 bool isLeapYear(int year);
+int daysInMonth(int month, int year);
 
 int main() {
-    char input;
+    string input;
+    int month;
+    int year;
+    const string monthNames[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
     do {
-        int year;
-        cout << "Enter a year or Q to quit: ";
-        cin >> input;
-        if (toupper(input) == 'Q') {
+        cout << "Enter a month and year or Q to quit: ";
+        getline(cin, input);
+        if (input == "Q" || input == "q") {
             break;
         }
-        cin >> year;
 
-        if (isLeapYear(year)) {
-            cout << "It is a leap year." << endl;
+        if (input.length() == 7) {
+            month = stoi(input.substr(0, 2));
+            year = stoi(input.substr(3, 4));
         }
-        else {
-            cout << "It is not a leap year." << endl;
+        else if (input.length() == 6) {
+            month = stoi(input.substr(0, 1));
+            year = stoi(input.substr(2, 4));
         }
+
+
+        if (month < 1 || month > 12) {
+            cout << "Invalid month.\n";
+            continue;
+        }
+
+        cout << monthNames[month - 1] << " " << year << " has " << daysInMonth(month, year) << " days." << endl;
     } while (true);
 
     return 0;
 }
 
+int daysInMonth(int month, int year) {
+    switch (month) {
+    case 2:
+        return isLeapYear(year) ? 29 : 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        return 30;
+    default:
+        return 31;
+    }
+}
 
 bool isLeapYear(int year) {
     if (year % 4 == 0) {
